42d1397a86b5c9f97d932c413d854e2747bbc0b5
S3O-ru <mail@s3o.ru>
2023-09-20 14:24:03 +0300

    git

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import 'normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './style/main.scss'
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    sladesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
