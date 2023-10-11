e36c389a0e77392285e7c23e651811e2b4efa026
S3O-ru <mail@s3o.ru>
2023-10-11 14:13:22 +0300

    pop up's

--- src/index.js
+++ src/index.js
@@ -69,3 +69,13 @@ var swiper = new Swiper(".offerSwiper", {
       }
     }
   });
+
+  var swiper = new Swiper(".categorySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    }
+  });
+
